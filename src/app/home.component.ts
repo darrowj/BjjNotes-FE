9b1905d4702a8c2248ed74f7f043347670840036
Jason Darrow <darrowj@gmail.com>
2017-11-29 07:06:27 -0500

    Broke out Home componenet from the detailed list component so visitiors not logged in will see a homepage and not notes that do not belong to them

---
+++ src/app/home.component.ts
@@ -0,0 +1,42 @@
+import {Component, OnInit, OnDestroy, AfterContentInit} from '@angular/core';
+import { TruncateModule } from 'ng2-truncate'
+import { ISubscription } from "rxjs/Subscription";
+import { NotesService} from './service/notes.service';
+import { Note} from './model/note';
+import 'rxjs/Rx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styles: []
+})
+export class HomeComponent implements OnInit, OnDestroy, AfterContentInit {
+
+  nbNotes: string;
+  private getNotesSubscription: ISubscription;
+  private getNoteCountSubscription: ISubscription;
+  notes: Note[];
+
+
+  constructor(private notesService: NotesService) { }
+
+  ngOnInit() {
+    console.log("ngOnInit() called");
+    this.getNotesSubscription = this.notesService.getHomeNotes().subscribe(notes => this.notes = notes.reverse());
+    this.getNoteCountSubscription = this.notesService.getHomeNoteCount().subscribe(count => this.nbNotes = count);
+  }
+
+  ngOnDestroy() {
+    console.log("ngOnDestroy() called");
+    this.getNotesSubscription.unsubscribe();
+    this.getNoteCountSubscription.unsubscribe();
+  }
+
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit() called");
+
+  }
+
+
+
+}
